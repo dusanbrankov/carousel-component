313ada0e004d15ee1e6389c0fa9422c3dab6dba0
Dušan Brankov <dusanbrankov@proton.me>
2025-05-25 12:29:40 +0200

    feat: add mouse and touch support

--- slide.js
+++ slide.js
@@ -6,16 +6,19 @@ const images = container.querySelectorAll("img");
 const nextButton = document.querySelector(".slide-next");
 const prevButton = document.querySelector(".slide-prev");
 
-nextButton.onclick = () => slide((pos) => pos - 1);
-prevButton.onclick = () => slide((pos) => pos + 1);
+nextButton.onclick = slideNext;
+prevButton.onclick = slidePrev;
+
+// Disable "next" button on first load
+handleButtonState(0);
 
 function slide(posUpdater) {
 	const currentPos = parseInt(container.style.getPropertyValue("--pos") || 0);
 	const newPos = posUpdater(currentPos);
-	handleButtonState(newPos);
 	if (newPos > 0 || Math.abs(newPos) >= images.length) {
 		return;
 	}
+	handleButtonState(newPos);
 	container.style.setProperty("--pos", newPos);
 }
 
@@ -32,4 +35,28 @@ function handleButtonState(pos) {
 	nextButton.setAttribute("aria-label", 
 		atEnd ? "Next image (unavailable)" : "Next image"
 	);
+}
+
+function slideNext() { slide((pos) => pos - 1); }
+function slidePrev() { slide((pos) => pos + 1); }
+
+root.onpointerdown = handleSwipeStart;
+root.onpointerup = handleSwipeEnd;
+
+const MIN_SWIPE_DISTANCE = 20;
+let startX = 0;
+
+function handleSwipeStart(e) {
+	if (e.target.hasPointerCapture(e.pointerId)) {
+		e.target.releasePointerCapture(e.pointerId);
+	}
+	startX = e.clientX;
+}
+
+function handleSwipeEnd(e) {
+	const endX = e.clientX;
+	const deltaX = endX - startX;
+	if (Math.abs(deltaX) > MIN_SWIPE_DISTANCE) {
+		deltaX < 0 ? slideNext() : slidePrev()
+	}
 }
