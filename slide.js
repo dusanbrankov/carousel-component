2f1bd733f236381a098a468c0e61dc9f6865931e
Dušan Brankov <dusanbrankov@proton.me>
2025-05-25 10:47:17 +0200

    feat(ux): indicate first/last image

--- slide.js
+++ slide.js
@@ -12,8 +12,24 @@ prevButton.onclick = () => slide((pos) => pos + 1);
 function slide(posUpdater) {
 	const currentPos = parseInt(container.style.getPropertyValue("--pos") || 0);
 	const newPos = posUpdater(currentPos);
+	handleButtonState(newPos);
 	if (newPos > 0 || Math.abs(newPos) >= images.length) {
 		return;
 	}
 	container.style.setProperty("--pos", newPos);
+}
+
+function handleButtonState(pos) {
+	atStart = pos === 0;
+	atEnd = Math.abs(pos) >= images.length - 1;
+
+	prevButton.disabled = atStart;
+	prevButton.setAttribute("aria-label", 
+		atStart ? "Previous image (unavailable)" : "Previous image"
+	);
+
+	nextButton.disabled = atEnd;
+	nextButton.setAttribute("aria-label", 
+		atEnd ? "Next image (unavailable)" : "Next image"
+	);
 }
